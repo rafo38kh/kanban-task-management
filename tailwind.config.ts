8829dc655b7699355dc0c37f6f0c47e4bfbd0628
Rafo Khachikyan <rafokhachikyan@Rafos-MacBook-Pro.local>
2024-04-12 21:33:16 -0700

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,19 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        kanbanPurpule: "#635FC7",
+        kanbanPurpuleHover: "#A8A4FF",
+        kanbanBlack: "#000112",
+        kanbanDarkGreyBG: "#20212C",
+        kanbanDarkGrey: "#2B2C37",
+        kanbanGrey: "#3E3F4E",
+        kanbanLightGrey: "#828FA3",
+        kanbanVeryLightGrey: "#E4EBFA",
+        kanbanLightGreyBG: "#F4F7FD",
+        kanbanRed: "#EA5555",
+        kanbanRedHover: "#FF9898",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
